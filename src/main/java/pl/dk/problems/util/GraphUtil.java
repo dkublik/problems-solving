b9e5901cff522db27d1ced616938a4f59cf29230
dkublik <dkublik@gmail.com>
2018-08-03 20:38:37 +0200

    missing graph util

---
+++ src/main/java/pl/dk/problems/util/GraphUtil.java
@@ -0,0 +1,21 @@
+package pl.dk.problems.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class GraphUtil {
+
+   public static void printGraph(Map<String, List<String>> graph) {
+       for (String node: graph.keySet()) {
+           System.out.println(node + ": " + Arrays.toString(graph.get(node).toArray()));
+       }
+   }
+
+    public static void printWeightedGraph(Map<String, List<Edge>> graph) {
+        for (String node: graph.keySet()) {
+            System.out.println(node + ": " + Arrays.toString(graph.get(node).toArray()));
+        }
+    }
+
+}
